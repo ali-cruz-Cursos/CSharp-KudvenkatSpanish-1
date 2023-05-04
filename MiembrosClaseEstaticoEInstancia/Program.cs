96426403d509e4d04d854ddadf03476e919f2642
Ali <ali.cruz@me.com>
2023-05-03 21:39:16 -0600

    Carga inicial

---
+++ MiembrosClaseEstaticoEInstancia/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiembrosClaseEstaticoEInstancia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo circulo = new Circulo(5);  // Se esta reservando memoria para _pi
+            float area = circulo.CalcularArea();
+
+            Console.WriteLine($"El area del circulo es: {area}");
+
+            Circulo circulo2 = new Circulo(6); // Se esta reservando memoria para _pi
+            float area2 = circulo2.CalcularArea();
+
+            Console.WriteLine($"El area del circulo2 es: {area2}");
+
+            // Llamo a un miembro estatico
+            // No cambian entre las diferentes instancias
+            Circulo.ImprimirArea();
+
+
+            Console.ReadLine();
+        }
+    }
+
+    class Circulo
+    {
+        static float _pi;
+        int _radio; // Es campo de instancia
+
+        public Circulo(int radio) // Constructor de instancia
+        {
+            Console.WriteLine("Constructor de instancia.\n");
+            _radio = radio;
+        }
+
+        // public static Circulo() // No se permiten modificadores de acceso
+        static Circulo()
+        {
+            Console.WriteLine("Constructor estatico.\n");
+            _pi = 3.1416f;
+        }
+
+        public static void ImprimirArea()
+        {
+            //
+        }
+
+        public float CalcularArea()
+        {
+            return Circulo._pi * (_radio * _radio);
+        }
+
+
+    }
+}
---
+++ TiposDatosNuleables/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposDatosNuleables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Value types: int, float, double, structs, enums, etc. 
+             *  Los value types por default son NO NULEABLES (usar ?)
+             *  
+             * Reference Types: Interfaces, Class, delegates, arrays, etc.
+             */
+            string nombre = null;
+            //int i = null; // No se puede convertir null a int. (No es de referencia)
+            int? i = null; //ahora es nulable
+            bool? esMayor = null;
+
+            if (esMayor == true)
+            {
+                Console.WriteLine("Es Mayor");
+            } else if (esMayor == false)
+            {
+                Console.WriteLine("No es mayor.");
+            } else
+            {
+                Console.WriteLine("Es null");
+            }
+
+            Console.WriteLine("\n\nBoletos camino largo\n");
+
+            int? boletosEnVenta = 100;
+            int boletosDisponibles;
+
+            if (boletosEnVenta == null)
+            {
+                boletosDisponibles = 0;
+            } else
+            {
+                boletosDisponibles = boletosEnVenta.Value;
+                //boletosDisponibles = (int)boletosEnVenta; // casting forzado.
+            }
+
+            Console.WriteLine($"Boletos disponibles {boletosDisponibles}\n");
+
+            Console.WriteLine("\n\nBoletos camino corto (NULL COALEASCING ??, operador de uso combinado)\n");
+
+            int? boletosEnVenta1 = 100;
+            int boletosDisponibles1 = boletosEnVenta1 ?? 0;
+
+
+
+            Console.WriteLine($"Boletos disponibles {boletosDisponibles1}");
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
