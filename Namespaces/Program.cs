d5d4a9a6447700df3ae627a5f4b5e9ce6467e0a5
Ali <ali.cruz@me.com>
2023-05-07 10:21:17 -0600

    Namespaces, creando Biblioteca de Clases para agregar referencia a proyectos dentro de la solución principal, Namespaces dentro de otros namespaces, alias en directiva using Video 18

---
+++ Namespaces/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+// Alias
+//using PAEA = ProyectoA.EquipoA;
+//using PAEB = ProyectoA.EquipoB;
+using System.Collections;
+using ProyectoA.EquipoA;
+using AliasProyectoA = ProyectoA.EquipoB;
+
+namespace Namespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Oranizar codigo
+            /*
+             * ProyectoA
+             *  EquipoA
+             *  EquipoB             *  
+             */
+
+            // ProyectoA.EquipoA.ClaseA.Imprimir();
+            // Se puede generar ambiguedad si se tiene el mismo nombre de Clase
+            // Se arregla definiendo la ruta completa de namespaces o creando alias
+            //PAEA.ClaseA.Imprimir();
+
+            //ProyectoA.EquipoB.ClaseA.Imprimir();
+            //PAEB.ClaseA.Imprimir();
+            AliasProyectoA.ClaseA.Imprimir();
+
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
+
+// Se agrega a una libreria de clases .net framework
+//namespace ProyectoA
+//{
+//    namespace EquipoA
+//    {
+//        class ClaseA
+//        {
+//            public static void Imprimir()
+//            {
+//                System.Console.WriteLine("Equipo A Clase A metodo Imprimir");
+//            }
+//        }
+//    }
+//}
+
+
+// Se mete a la libreria creada por separado
+//namespace ProyectoA
+//{
+//    namespace EquipoB
+//    {
+//        class ClaseA
+//        {
+//            public static void Imprimir()
+//            {
+//                Console.WriteLine("Equipo B Clase B imprimir");
+//            }
+//        }
+
+//    }
+//}
---
+++ ProyectoA.EquipoA/ClaseA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoA.EquipoA
+{
+    public class ClaseA
+    {
+        public static void Imprimir()
+        {
+            System.Console.WriteLine("Equipo A Clase A metodo Imprimir");
+        }
+    }
+}
