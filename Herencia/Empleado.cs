9ca2d130dd24a7315ccd404bbb13e1a4d3f5feef
Ali <ali.cruz@me.com>
2023-05-07 11:37:18 -0600

    Antivirus me lo detecta como Nombre: VHO:Trojan.Win32.Sdum.gen MD5: 53E2744E541E99EBD8F612203A97CCB3 justo asi como esta el codigo, si comento la linea 22 Console.WriteLine($'Salario: de Program.cs entonces ya no es detectado como amenaza.

---
+++ Herencia/Empleado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    public class Empleado
+    {
+        // Se debería de usar propiedades en lugar de campos publicos.
+        public string Nombre;
+        public string Apellido;
+        public string  Email;
+
+        public void ImprimirNombreCompleto()
+        {
+            Console.WriteLine($"Nombre Completo: {Nombre} {Apellido}");
+
+        }
+    }
+}
---
+++ Herencia/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Video 21 Herencia
+            // Clase Base
+            // Clase derivada
+
+            EmpleadoTiempoCompleto empleadoTiempoCompleto = new EmpleadoTiempoCompleto();
+            empleadoTiempoCompleto.Nombre = "Ali";
+            empleadoTiempoCompleto.Apellido = "Cruz";
+            empleadoTiempoCompleto.SalarioAnual = 110_000;
+            empleadoTiempoCompleto.ImprimirNombreCompleto();
+            Console.WriteLine($"Salario: {empleadoTiempoCompleto.SalarioAnual}.");
+
+            Console.ReadLine();
+
+        }
+    }
+}
