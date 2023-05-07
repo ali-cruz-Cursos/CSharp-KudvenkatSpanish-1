10337e96e4faed0d9969dff0b5bec3fe22dac3d2
Ali <ali.cruz@me.com>
2023-05-07 06:46:00 -0600

    Parametros de metodos por valor, referencia (in, ref, out), Tuplas, Retornar mas de un valor, Pasar varios argumentos con params Video 17

---
+++ Tipos-Parametros-Metodo/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Tipos_Parametros_Metodo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int i2 = 0;
+
+            MetodoSimple(i); // Se pasa como valor, se pasa una copia
+            Console.WriteLine($"i {i}");
+
+            MetodoPorReferencia(ref i2); // Se pasa por referencia.            
+            Console.WriteLine($"i2 {i2}");
+
+            int totSum = 0;
+            int totMult = 0;
+
+            Calcular(3, 5, out totSum, out totMult);
+            Console.WriteLine($"Tot sum {totSum} totMul {totMult}");
+
+            var res = Calcular2(5, 10);
+
+            totSum = res.Item1;
+            totMult = res.Item2;
+            Console.WriteLine($"Tot sum {totSum} totMul {totMult}");
+
+            int tipoCambio = 19;
+            int dolares = 100;
+            int pesos = ConvertirAPesos(dolares, in tipoCambio); // tipoCambio se para por referencia
+
+            Console.WriteLine($"Tipo Cambio {tipoCambio} Dolares: {dolares}. Convertido {pesos}");
+
+            int[] arregloNumeros = new int[3] { 101, 102, 103 };
+
+            /* Al usar un arreglo sin la palabra params entonces se vuelve
+             * obligatorio el argumento.
+             * Si se usa params es opcional el argumento
+             */
+            MetodoConParams("array", arregloNumeros);
+
+            MetodoConParams("valores separados por coma", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+            Console.ReadLine();
+        }
+
+        // Se pasa como valor
+        public static void MetodoSimple(int j)
+        {
+            j = 101;
+        }
+
+
+        // Se pasa por referencia
+        public static void MetodoPorReferencia(ref int i2)
+        {
+            i2 = 150;
+        }
+
+
+        public static void Calcular(int x, int y, out int suma, out int multi)
+        {
+            suma = x + y;
+            multi = x * y;
+        }
+
+        // Tuplas
+        public static (int, int) Calcular2(int x, int y)
+        {
+            int suma = x + y;
+            int mult = x * y;
+            return (suma, mult);
+        }
+
+        public static int ConvertirAPesos(int dolares, in int tipoCambio)
+        {
+            //tipoCambio = 25; // Es de solo lectura al usar "in" se pasa por referencia
+            return dolares * tipoCambio;
+        }
+
+
+        // params debe ser el último elemento en la lista de parametros y solo puede existir uno solo.
+        public static void MetodoConParams(string value, params int[] numeros)
+        {
+            Console.WriteLine($"Total de elementos en el arreglo: {numeros.Length} (Se pasan como {value})");
+
+            foreach (int num in numeros)
+            {
+                Console.WriteLine($"Elemento {num}");
+            }
+        }
+
+    }
+}
---
+++ Tipos-Parametros-Metodo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Tipos-Parametros-Metodo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tipos-Parametros-Metodo")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a9823db3-0415-4cf0-8f31-34df5ab32238")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
